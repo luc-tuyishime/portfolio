9dd0f2d56b8aca4e5f61f7006c56e13be79727e8
luc-tuyishime <luctunechi45@gmail.com>
2022-01-10 19:09:59 +0200

    add helper folder

---
+++ helper/apiCallHook.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import axios from axios;
+
+///
+
+axios.defaults.baseURL = 'http://jsonplaceholder.typicaode.com';
+
+const useAxios = ({ url, method, body = null, headers = null}) => {
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const fetchData = () => {
+        axios[method](url, JSON.parse(headers), JSON.parse(body)).then((res) => {
+            setResponse(res.data)
+        }).catch((err) => {
+            setError(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [method, url, body, headers]);
+
+    return { response, error, loading}
+}
+
+export default useAxios
